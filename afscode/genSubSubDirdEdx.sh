1c795a4484387efec187c0e46b8adaa3deeb68b0
Kevin <kpm82@cornell.edu>
2015-08-06 13:25:31 +0200

    working now with new dedx and tkbuilding plots

--- afscode/genSubSubDirdEdx.sh
+++ afscode/genSubSubDirdEdx.sh
@@ -7,12 +7,12 @@ printf "<html>\n" > index.html
 #printf "     <body text=\"#000000\" bgcolor=\"#FFFFFF\" link=\"#FFFF00\" vlink=\"#FF00FF\">\n" >> index.html  
 printf "  <title>dEdx Distributions</title>\n" >> index.html
 printf "     <body>\n" >> index.html  
-printf "          <a href=\"%s/%s/PO\">dEdx DQMHarm2PO</a>\n" ${web} ${release} >> index.html
+printf "          <a href=\"%s/%s/dEdx/PO\">dEdx DQMHarm2PO</a>\n" ${web} ${release} >> index.html
 printf "          <br />\n" >> index.html    
-printf "          <a href=\"%s/%s/SO\">dEdx DQMHarm2SO</a>\n" ${web} ${release} >> index.html
+printf "          <a href=\"%s/%s/dEdx/SO\">dEdx DQMHarm2SO</a>\n" ${web} ${release} >> index.html
 printf "          <br />\n" >> index.html    
-printf "          <a href=\"%s/%s/SP\">dEdx DQMHarm2SP</a>\n" ${web} ${release} >> index.html
+printf "          <a href=\"%s/%s/dEdx/SP\">dEdx DQMHarm2SP</a>\n" ${web} ${release} >> index.html
 printf "          <br />\n" >> index.html    
-printf "          <a href=\"/%s/%s/HitInfo\">dEdxHitInfo</a>\n" ${web} ${release} >> index.html
+printf "          <a href=\"/%s/%s/dEdx/HitInfo\">dEdxHitInfo</a>\n" ${web} ${release} >> index.html
 printf "     </body>\n" >> index.html
 printf "</html>\n" >> index.html
