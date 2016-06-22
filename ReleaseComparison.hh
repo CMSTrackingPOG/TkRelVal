c9d01135a128858dcb5a72ad97a9f57e919059c8
Kevin <kpm82@cornell.edu>
2016-06-22 09:32:03 +0200

    updates

--- ReleaseComparison.hh
+++ ReleaseComparison.hh
@@ -20,13 +20,13 @@
 
 void V1_V2_trkComparison(const string fileName1, const string fileName2, 
 			 const TString directory, const bool full);
-void CMSLumi(TCanvas *& canv, const Int_t iPosX, const Int_t tev, const Double_t lumi);
+void CMSLumi(TCanvas *& canv, const Int_t iPosX, const Int_t tev, const Double_t lumi, const Bool_t collisions);
 bool createTH1FPlot (const TString hname, const TString dirname, TFile *& V1file, const TString runstring1, 
 		     const TString relstring1, TFile *& V2file, const TString runstring2, 
 		     const TString relstring2, TCanvas *& canvas, const Double_t V1_integral, 
-		     const Double_t V2_integral, TString outdir, const Double_t lumi, const Int_t tev);
+		     const Double_t V2_integral, TString outdir, const Double_t lumi, const Int_t tev, const Bool_t collisions);
 bool createTProfPlot(const TString hname, const TString dirname, TFile *& V1file, const TString runstring1, 
 		     const TString relstring1, TFile *& V2file, const TString runstring2, 
 		     const TString relstring2, TCanvas *& canvas, const Double_t V1_integral, 
-		     const Double_t V2_integral, TString outdir, const Double_t lumi, const Int_t tev);
+		     const Double_t V2_integral, TString outdir, const Double_t lumi, const Int_t tev, const Bool_t collisions);
 void setTDRStyle(TStyle *& tdrStyle);
--- makeValidationPlots.sh
+++ makeValidationPlots.sh
@@ -9,7 +9,7 @@ full=${5:false} # if true, do all plots, otherwise, just essential plots
 
 refFile=$(ls *"${run}"*"${sample}"*"${rel_old}"*)
 newFile=$(ls *"${run}"*"${sample}"*"${rel_new}"*)
-#release=CMSSW_8_0_7_HLTnewcond_vs_PRref_"${run}"_"${sample}"
+#release=CMSSW_808p1_HLTnewcond_vs_HLTref_"${run}"_"${sample}"
 release=CMSSW_"${rel_new}"_vs_"${rel_old}"_"${run}"_"${sample}"
 
 directory=/afs/cern.ch/cms/Physics/tracking/validation/DATA/${release}
