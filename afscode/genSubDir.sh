ca63aefeb58921ad44121510b588d24b3b9d0ee7
Kevin McDermott <kpm82@cornell.edu>
2017-08-29 09:29:38 +0200

    add top level dirs for V0 and HP dzPV0p1

--- afscode/genSubDir.sh
+++ afscode/genSubDir.sh
@@ -10,11 +10,13 @@ printf "     <body>\n" >> index.html
 printf "        <h2>%s Collisions Validation </h2>\n" ${release} >> index.html
 printf "           <ul>\n" >> index.html
 printf "              <li/><a href=\"%s/%s/genTks\">General Tracks Plots</a>\n" ${web} ${release} >> index.html
+printf "              <li/><a href=\"%s/%s/HPTks_dzPV0p1\">High Purity (dz PV 0 < p < 1) Tracks Plots</a>\n" ${web} ${release} >> index.html
 printf "              <li/><a href=\"%s/%s/HPTks_0to1\">High Purity (0 < pT < 1 GeV) Tracks Plots</a>\n" ${web} ${release} >> index.html
 printf "              <li/><a href=\"%s/%s/HPTks_gt1\">High Purity (pT > 1 GeV) Tracks Plots</a>\n" ${web} ${release} >> index.html
 printf "              <li/><a href=\"%s/%s/PackCand\">Packed Candidate Plots</a>\n" ${web} ${release} >> index.html
 printf "              <li/><a href=\"%s/%s/SiStrip\">SiStrip Plots</a>\n" ${web} ${release} >> index.html
 printf "              <li/><a href=\"%s/%s/dEdx\">dEdx Plots</a>\n" ${web} ${release} >> index.html
+printf "              <li/><a href=\"%s/%s/V0\">V0 Monitoring Plots</a>\n" ${web} ${release} >> index.html
 printf "              <li/><a href=\"%s/%s/OfflinePV\">Offline Primary Vertices Plots</a>\n" ${web} ${release} >> index.html
 printf "              <li/><a href=\"%s/%s/PV_HPTks\">Tracking High Purity Tracks (0 < pT < 1 GeV) Primary Vertices Plots</a>\n" ${web} ${release} >> index.html
 printf "           </ul>\n" >> index.html
