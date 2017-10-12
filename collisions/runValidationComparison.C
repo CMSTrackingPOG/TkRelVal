13302a3afde4c6aaf3bd09f0b53085614d86a5c5
Kevin <kpm82@cornell.edu>
2017-10-12 13:59:48 +0200

    further pruning

--- collisions/runValidationComparison.C
+++ collisions/runValidationComparison.C
@@ -1,26 +1,8 @@
-#include "TString.h"
-#include "TSystem.h"
-#include "TROOT.h"
 #include "ReleaseComparison.cpp+"
-
-void setupcpp11() // customize ACLiC's behavior ...
-{
-  TString o;
-  // customize MakeSharedLib
-  o = TString(gSystem->GetMakeSharedLib());
-  o = o.ReplaceAll(" -c ", " -std=c++0x -c ");
-  gSystem->SetMakeSharedLib(o.Data());
-  // customize MakeExe
-  o = TString(gSystem->GetMakeExe());
-  o = o.ReplaceAll(" -c ", " -std=c++0x -c ");
-  gSystem->SetMakeExe(o.Data());
-} 
 
 void runValidationComparison(const char * refFile, const char * newFile, 
 			     const TString directory, const TString full) 
 {
-  setupcpp11(); 
-
   bool fullbool = full.Contains("true",TString::kExact);
   V1_V2_trkComparison(refFile,newFile,directory,fullbool);
 }
