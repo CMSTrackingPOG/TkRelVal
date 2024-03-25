6c3aed0dd50001b04ae0d989d47705ca0aa61c5f
acmbulla <a.bulla@campus.unimib.it>
2024-03-25 17:12:08 +0100

    bug report

--- getFiles.sh
+++ getFiles.sh
@@ -13,35 +13,37 @@ echo "Getting sample: $sample"
 wget -e robots=off --wait 1 -r -l1 -nd -np "https://cmsweb.cern.ch/dqm/relval/data/browse/ROOT/RelValData/$web_dir/" -A "*$run*$sample*$rel*$add_key*$optional_arg*root" --no-check-certificate --certificate ~/.globus/html_cert/myCert.pem --private-key ~/.globus/html_cert/myCert.key
 
 
-if [ ${sample} == "Cosmics" ] ; then
-    if ls *${sample}*.root 1> /dev/null 2>&1; then
-        mv *${sample}*.root cosmics/
-    else
-        echo "File not found for sample: $sample"
-    fi
-else
-    if ls *${run}*${sample}*.root 1> /dev/null 2>&1; then
-        cd collisions/DQM
-        if [ ! -d ${web_dir} ] ; then    
-            mkdir ${web_dir}
-        fi 
-        cd ${web_dir}
-        relval=${rel}
-        if [ ! -d ${rel} ] ; then    
-            mkdir ${rel}
-        fi
-        cd ${rel}
-        if [ ! -d ${run} ] ; then    
-            mkdir ${run}
-        fi
-        cd ${run}
-        if [ ! -d ${sample} ] ; then 
-            mkdir ${sample}
-        fi
-        cd ../../../../../
+# if [ ${sample} == "Cosmics" ] ; then
+#     if ls *${sample}*.root 1> /dev/null 2>&1; then
+#         mv *${sample}*.root cosmics/
+#     else
+#         echo "File not found for sample: $sample"
+#     fi
+# else
+#     if ls *${run}*${sample}*.root 1> /dev/null 2>&1; then
+#         cd collisions/DQM
+#         if [ ! -d ${web_dir} ] ; then    
+#             mkdir ${web_dir}
+#         fi 
+#         cd ${web_dir}
+#         relval=${rel}
+#         if [ ! -d ${rel} ] ; then    
+#             mkdir ${rel}
+#         fi
+#         cd ${rel}
+#         if [ ! -d ${run} ] ; then    
+#             mkdir ${run}
+#         fi
+#         cd ${run}
+#         if [ ! -d ${sample} ] ; then 
+#             mkdir ${sample}
+#         fi
+#         cd ../../../../../
 
-        mv *${run}*${sample}*.root collisions/DQM/${web_dir}/${rel}/${run}/${sample}/
-    else
-        echo "File not found for sample: $sample"
-    fi
-fi
+#         mv *${run}*${sample}*.root collisions/DQM/${web_dir}/${rel}/${run}/${sample}/
+#     else
+#         echo "File not found for sample: $sample"
+#     fi
+# fi
+
+mv *root collisions/DQM
